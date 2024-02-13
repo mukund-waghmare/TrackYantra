d7193c8e8b30a72d6e84e979b23622276a1e62eb
mukund-waghmare <mukundwaghmare4950@gmail.com>
2024-02-13 21:52:50 +0530

    added adminControllers Method

--- src/main/java/com/ty/TrackYantra/dao/AdminDao.java
+++ src/main/java/com/ty/TrackYantra/dao/AdminDao.java
@@ -1,11 +1,18 @@
 package com.ty.TrackYantra.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ty.TrackYantra.dto.Admin;
 import com.ty.TrackYantra.dto.Designation;
+import com.ty.TrackYantra.dto.Employee;
+import com.ty.TrackYantra.dto.ReportingManager;
 import com.ty.TrackYantra.repository.AdminRepository;
+import com.ty.TrackYantra.repository.EmployeeRepository;
+import com.ty.TrackYantra.repository.RepotingManagerRepository;
 
 @Repository
 public class AdminDao {
@@ -13,6 +20,12 @@ public class AdminDao {
 	
 	@Autowired
 	AdminRepository adminRepositoryObject;
+	
+	@Autowired
+	RepotingManagerRepository repotingManagerRepositoryObject;
+	
+	@Autowired
+	EmployeeRepository employeeRepository;
 	
 	
 	public Admin saveAdmin(Admin admin)
@@ -22,9 +35,62 @@ public class AdminDao {
 	}
 	
 	
-	public Admin getAdminByDesignation(Designation designation)
+	public Admin getAdminByDesignation(Designation passedDesignation)
 	{
-		return adminRepositoryObject.findAdminByDesignation(designation);
+		 Admin admin=adminRepositoryObject.findAdminByDesignation(passedDesignation);
+		 if(admin!=null)
+		 {
+			 return admin;
+		 }
+		 return null;
 	}
+	
+	
+	public Admin getAdminById(int passedAdminId)
+	{
+		 Optional<Admin> admin=adminRepositoryObject.findById(passedAdminId);
+		 if(admin.isEmpty())
+		 {
+			 return admin.get();
+		 }
+		 return null;
+	}
+	
+	
+	public Admin getAdminByEmail(String passedEmail)
+	{
+		Admin admin=adminRepositoryObject.findByAdminEmail(passedEmail);
+		
+		if(admin!=null)
+		{
+			return admin;
+		}
+		return null;
+	
+	}
+	
+	public List<ReportingManager> getAllReportingManager()
+	{
+		List<ReportingManager> reportinManagerList=repotingManagerRepositoryObject.findAll();
+		
+		if(reportinManagerList!=null)
+		{
+		
+			return reportinManagerList;
+		}
+		return null;
+	}
+	
+	public List<Employee> getEmployeeByReportingManagerID(int passedReportingManagerId)
+	{
+		Optional<ReportingManager> reportingManager=repotingManagerRepositoryObject.findById(passedReportingManagerId);
+		if(reportingManager!=null)
+		{
+			return reportingManager.get().getEmployeeList();
+		}
+		return null;
+	}
+	
+	
 
 }
---
+++ src/main/java/com/ty/TrackYantra/service/AdminServiceImplimentatation.java
@@ -0,0 +1,141 @@
+package com.ty.TrackYantra.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ty.TrackYantra.dao.AdminDao;
+import com.ty.TrackYantra.dto.Admin;
+import com.ty.TrackYantra.dto.Designation;
+import com.ty.TrackYantra.dto.Employee;
+import com.ty.TrackYantra.dto.ReportingManager;
+import com.ty.TrackYantra.dto.ResponseStructure;
+
+@Service
+public class AdminServiceImplimentatation implements AdminService {
+	
+	@Autowired
+	AdminDao adminDaoObject;
+
+	@Override
+	public ResponseEntity<ResponseStructure<Admin>> saveAdmin(Admin passedAdmin) {
+		// TODO Auto-generated method stub
+		Admin admin=adminDaoObject.saveAdmin(passedAdmin);
+		if(admin!=null)
+		{
+			ResponseStructure<Admin> resp= new ResponseStructure<Admin>();
+			resp.setMessage("Success");
+			resp.setStatusCode(HttpStatus.ACCEPTED.value());
+			resp.setData(admin);
+			
+			return new ResponseEntity<ResponseStructure<Admin>>(resp,HttpStatus.ACCEPTED);
+			
+			
+		}
+		//throw exception 
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<ResponseStructure<Admin>> getAdminByDesignation(Designation passedDesignation) {
+		// TODO Auto-generated method stub
+		Admin admin=adminDaoObject.getAdminByDesignation(passedDesignation);
+		if(admin!=null)
+		{
+			ResponseStructure<Admin> resp= new ResponseStructure<Admin>();
+			resp.setMessage("Success");
+			resp.setStatusCode(HttpStatus.ACCEPTED.value());
+			resp.setData(admin);
+			return new ResponseEntity<ResponseStructure<Admin>>(resp,HttpStatus.ACCEPTED);
+			
+		}
+		//throw Admin Doesnot Exist For Given Designation
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<ResponseStructure<Admin>> getAdminById(int passedId) {
+		
+		Admin admin=adminDaoObject.getAdminById(passedId);
+		if(admin!=null)
+		{
+			ResponseStructure<Admin> resp= new ResponseStructure<Admin>();
+			resp.setMessage("Success");
+			resp.setStatusCode(HttpStatus.ACCEPTED.value());
+			resp.setData(admin);
+			return new ResponseEntity<ResponseStructure<Admin>>(resp,HttpStatus.ACCEPTED);
+			
+		}
+
+		//throw Admin Doesnot Exist For Given Id
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<ResponseStructure<Admin>> getAdminByEmail(String passedEmail) {
+		// TODO Auto-generated method stub
+		
+		Admin admin=adminDaoObject.getAdminByEmail(passedEmail);
+		
+		if(admin!=null)
+		{
+			ResponseStructure<Admin> resp= new ResponseStructure<Admin>();
+			resp.setMessage("Success");
+			resp.setStatusCode(HttpStatus.ACCEPTED.value());
+			resp.setData(admin);
+			return new ResponseEntity<ResponseStructure<Admin>>(resp,HttpStatus.ACCEPTED);
+			
+		}
+		
+
+		//throw Admin Doesnot Exist For Given Email
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<ResponseStructure<List<ReportingManager>>> getAllReportingManager() {
+		// TODO Auto-generated method stub
+		
+		List<ReportingManager> repoManagerList=adminDaoObject.getAllReportingManager();
+		
+		if(repoManagerList!=null)
+		{
+			ResponseStructure<List<ReportingManager>> resp= new ResponseStructure<List<ReportingManager>>();
+			resp.setMessage("Success");
+			resp.setStatusCode(HttpStatus.ACCEPTED.value());
+			resp.setData(repoManagerList);
+			return new ResponseEntity<ResponseStructure<List<ReportingManager>>>(resp,HttpStatus.ACCEPTED);
+			
+		}
+		
+		// throw no Reporting Manager Found
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<ResponseStructure<List<Employee>>> getEmployeeByReportingManagerID(
+			int passedReportingManagerId) {
+		// TODO Auto-generated method stub
+		
+		List<Employee> employeeList=adminDaoObject.getEmployeeByReportingManagerID(passedReportingManagerId);
+		
+		if(employeeList!=null)
+		{
+			ResponseStructure<List<Employee>> resp= new ResponseStructure<List<Employee>>();
+			resp.setMessage("Success");
+			resp.setStatusCode(HttpStatus.ACCEPTED.value());
+			resp.setData(employeeList);
+			return new ResponseEntity<ResponseStructure<List<Employee>>>(resp,HttpStatus.ACCEPTED);
+			
+			
+		}
+			
+		// manager does not exist for givenId
+		return null;
+	}
+	
+
+}
