8ad0bd72c50dace6d0f5813176b7ebb1064b6924
junedgithub <junedmodi4@gmail.com>
2024-02-15 17:20:54 +0530

    method getreportingManagerByEmailAndPassWord Added

--- src/main/java/com/ty/TrackYantra/controller/ReportingManagerController.java
+++ src/main/java/com/ty/TrackYantra/controller/ReportingManagerController.java
@@ -11,6 +11,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class ReportingManagerController {
@@ -35,8 +38,8 @@ public class ReportingManagerController {
     @Operation(description = "Save Reporting Manager",summary = "save reporting manager")
     @ApiResponses(value = {@ApiResponse(description = "Save Reporting Manager",responseCode = "200"),@ApiResponse(description = "Not Save",responseCode = "400")})
     @PostMapping("/addReportingManager/adminEmail/{adminEmail}/adminPassword/{adminPassword}")
-    public ResponseEntity<ResponseStructure<ReportingManager>> saveReportingManager(@PathVariable String adminEmail,@PathVariable String adminPassword,@RequestBody ReportingManager reportingManager){
-        return reportingManagerService.saveReportingManager(adminEmail,adminPassword,reportingManager);
+    public ResponseEntity<ResponseStructure<ReportingManager>> saveReportingManager(@PathVariable String adminEmail, @PathVariable String adminPassword, @ModelAttribute ReportingManager reportingManager,@RequestParam("image") MultipartFile file) throws IOException {
+        return reportingManagerService.saveReportingManager(adminEmail,adminPassword,reportingManager,file);
     }
 
     @Operation(description = "Update Reporting Manager Password",summary = "update reporting email manager by id")
@@ -46,5 +49,10 @@ public class ReportingManagerController {
         return reportingManagerService.updateReportingManagerPasswordById(reportingManagerId,adminEmail,adminPassword,reportingManager);
     }
 
+    @Operation(description = "Get Reporting Manager by Email And Password",summary = "get reporting email manager by email and password")
+    @ApiResponses(value = {@ApiResponse(description = "Get Reporting Manager By Email And Password",responseCode = "200"),@ApiResponse(description = "Not Found",responseCode = "404")})
+    @GetMapping("/getReportingManagerByEmailAndPassword/reportingManagerEmail/{reportingManagerEmail}/reportingManagerPassword/{reportingManagerPassword}")
+    public ResponseEntity<ResponseStructure<ReportingManager>> getReportingManagerByEmailAndPassword(@PathVariable String reportingManagerEmail,@PathVariable String reportingManagerPassword){
+        return reportingManagerService.getReportingManagerByEmailAndPassword(reportingManagerEmail,reportingManagerPassword);
+    }
 }
-
