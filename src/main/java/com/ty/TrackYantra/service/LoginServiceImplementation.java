44bc7547a28202248877244b2dff2628f5de8247
truptilingaraju <ltrupti355@gmail.com>
2024-02-15 17:49:57 +0530

    login implementation

--- src/main/java/com/ty/TrackYantra/service/LoginServiceImplementation.java
+++ src/main/java/com/ty/TrackYantra/service/LoginServiceImplementation.java
@@ -1,5 +1,8 @@
 package com.ty.TrackYantra.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,8 @@ import com.ty.TrackYantra.dto.Employee;
 import com.ty.TrackYantra.dto.Login;
 import com.ty.TrackYantra.dto.ReportingManager;
 import com.ty.TrackYantra.dto.ResponseStructure;
+import com.ty.TrackYantra.exception.InvalidAdminCredentials;
+import com.ty.TrackYantra.exception.ReportingManagerNotFound;
 
 @Service
 public class LoginServiceImplementation implements LoginService{
@@ -32,23 +37,54 @@ public class LoginServiceImplementation implements LoginService{
 	
 	@Autowired
 	private LoginDao loginDao;
-//	
+	
 //	@Override
 //	public ResponseEntity<ResponseStructure<Login>> saveLogin(Login login) {
 //		ResponseStructure<Login> structure=new ResponseStructure<>();
 //		if(Designation.ADMIN==login.getDesignation()) {
 //			Admin admin=adminDao.getAdminByEmailAndPassword(login.getEmail(), login.getPassword());
 //			if(admin!=null) {
+//				login.setAdmin(admin);
 //				Login login1=loginDao.saveLogin(login);
+//				List<Login> logins=admin.getLogins();
+//				if(logins==null) {
+//					logins=new ArrayList<>();
+//				}
+//				logins.add(login1);
+//				admin.setLogins(logins);
+//				adminDao.saveAdmin(admin);
 //				structure.setStatusCode(HttpStatus.CREATED.value());
 //				structure.setMessage("login saved successfully");
 //				structure.setData(login1);
 //				
 //				return new ResponseEntity<ResponseStructure<Login>>(structure,HttpStatus.CREATED);
 //			}
+//			else {
+//				throw new InvalidAdminCredentials("invalid credentials");
+//			}
 //		}
 //		else if(Designation.REPORTINGMANAGER==login.getDesignation()) {
-//			ReportingManager manager=managerDao;
+//			ReportingManager manager=managerDao.getReportingManagerByEmailAndPassword(ogin.getEmail(), login.getPassword());
+//			if(manager!=null) {
+//				login.setManager(manager);
+//				Login login1=loginDao.saveLogin(login);
+//				List<Login> logins=manager.getLogins();
+//				if(logins==null) {
+//					logins=new ArrayList<>();
+//				}
+//				logins.add(login1);
+//				manager.setLogins(logins);
+//				managerDao.saveReportingManager(manager);
+//				structure.setStatusCode(HttpStatus.CREATED.value());
+//				structure.setMessage("login saved successfully");
+//				structure.setData(login1);
+//				
+//				return new ResponseEntity<ResponseStructure<Login>>(structure,HttpStatus.CREATED);
+//	
+//			}
+//			else {
+//				throw new ReportingManagerNotFound("invalid credentilas");
+//			}
 //		}
 //		
 //		else  {
